d11302f62a11c41ed35b1cb1a355e28f94af29a7
RjGunner111 <rj.kumar.rk959@gmail.com>
2022-06-02 10:53:20 -0500

    Update Changelog.Designer.cs

--- pyart 4.0/Changelog.Designer.cs
+++ pyart 4.0/Changelog.Designer.cs
@@ -39,9 +39,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(12, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.Size = new System.Drawing.Size(178, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "History of versions:";
             // 
